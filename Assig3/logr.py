01bb8509ec8b9916da46467ef91518a1a87c6c0e
bb-blud <rsalo001@gmail.com>
2016-10-02 20:40:23 -0400

    After having difficulties with tensorflow on the ipython notebook I decided to prototype with a regular .py file

---
+++ Assig3/logr.py
@@ -0,0 +1,106 @@
+# These are all the modules we'll be using later. Make sure you can import them
+# before proceeding further.
+from __future__ import print_function
+import numpy as np
+import tensorflow as tf
+from six.moves import cPickle as pickle
+from time import time
+########
+
+
+# Reload data 
+pickle_file = '../pickled/notMNIST.pickle'
+
+with open(pickle_file, 'rb') as f:
+  save = pickle.load(f)
+  train_dataset = save['train_dataset']
+  train_labels = save['train_labels']
+  valid_dataset = save['valid_dataset']
+  valid_labels = save['valid_labels']
+  test_dataset = save['test_dataset']
+  test_labels = save['test_labels']
+  del save  # hint to help gc free up memory
+  print('Training set', train_dataset.shape, train_labels.shape)
+  print('Validation set', valid_dataset.shape, valid_labels.shape)
+  print('Test set', test_dataset.shape, test_labels.shape)
+
+
+
+# Flatten images to vectors of dim 784
+image_size = 28
+num_labels = 10
+
+def reformat(dataset, labels):
+  dataset = dataset.reshape((-1, image_size * image_size)).astype(np.float32)
+  # Map 2 to [0.0, 1.0, 0.0 ...], 3 to [0.0, 0.0, 1.0 ...]
+  labels = (np.arange(num_labels) == labels[:,None]).astype(np.float32)
+  return dataset, labels
+train_dataset, train_labels = reformat(train_dataset, train_labels)
+valid_dataset, valid_labels = reformat(valid_dataset, valid_labels)
+test_dataset, test_labels = reformat(test_dataset, test_labels)
+print('Training set', train_dataset.shape, train_labels.shape)
+print('Validation set', valid_dataset.shape, valid_labels.shape)
+print('Test set', test_dataset.shape, test_labels.shape)
+
+# Accuracy helper function
+def accuracy(predictions, labels):
+  return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1))
+          / predictions.shape[0])
+
+############################################################### 
+#Problem 1: Introduce L2 regularization to Logistic Regression
+
+graph = tf.Graph()
+with graph.as_default():
+    # Parameters
+    learning_rate = 0.01
+    training_epochs = 25
+    batch_size = 128
+
+    # tf Graph Input
+    X = tf.placeholder(tf.float32, [None, 784]) # mnist data image of shape 28*28=784
+    y = tf.placeholder(tf.float32, [None, 10]) # 0-9 digits recognition => 10 classes
+    
+
+    # Set model weights
+    W = tf.Variable(tf.zeros([784, 10]))
+    b = tf.Variable(tf.zeros([10]))
+
+    # Construct model
+    pred = tf.nn.softmax(tf.matmul(X, W) + b) # Softmax
+
+    # Minimize error using cross entropy    
+    loss = tf.reduce_mean(-tf.reduce_sum(y*tf.log(pred), reduction_indices=1)) #+ tf.nn.l2_loss(W)
+    # Gradient Descent
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+
+    # Predictions
+    valid_prediction = tf.nn.softmax(tf.matmul(valid_dataset, W) + b)
+    test_prediction  = tf.nn.softmax(tf.matmul(test_dataset, W) + b)
+
+
+num_steps = 3001    
+start = time()
+with tf.Session(graph=graph) as session:
+    
+  tf.initialize_all_variables().run()
+  print("Initialized")
+  
+  for step in range(num_steps):
+    offset = (step * batch_size) % (train_labels.shape[0] - batch_size)
+    # Generate a minibatch.
+    batch_data = train_dataset[offset:(offset + batch_size),  :]
+    batch_labels = train_labels[offset:(offset + batch_size), :]
+
+    # Prepare  dictionary
+    _, l, predictions = session.run(
+      [optimizer, loss, pred], feed_dict={X : batch_data,
+                                          y : batch_labels})
+    if (step % 500 == 0):
+      print("Minibatch loss at step %d: %f" % (step, l))
+      print("Minibatch accuracy: %.1f%%" % accuracy(predictions, batch_labels))
+      print("Validation accuracy: %.1f%%" % accuracy(
+        valid_prediction.eval(), valid_labels))
+  print("Test accuracy: %.1f%%" % accuracy(test_prediction.eval(), test_labels))
+
+print( time() - start )
